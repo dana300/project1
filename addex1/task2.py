5988a35e53a05fae40a3cde259755db7713f8c63
dana300 <64768549+dana300@users.noreply.github.com>
2021-06-06 13:16:09 +0300

    Add files via upload

---
+++ addex1/task2.py
@@ -0,0 +1,12 @@
+def naive_mul(x, y, res=0):
+    for _ in range(y):
+        res += x
+    return res
+
+def test_naive_mul():
+    for x in range(101):
+        for y in range(101):
+            assert naive_mul(x, y) == x * y
+    print("All tests passed")
+
+test_naive_mul()
---
+++ addex3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="packwithdata",
+    version="1.0.0",
+    author="Dana",
+    author_email="08dana-01@mail.ru",
+    packages=["packwithdata"],
+    description="This is a test package for python practice"
+)
